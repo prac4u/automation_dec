7b760fba1fe41d71719b9bdde31cc26c830356e2
pra4u <palkar.p@gamail.com>
2023-02-12 09:23:00 +0530

    corrected test cases

--- src/main/java/util/Constants.java
+++ src/main/java/util/Constants.java
@@ -6,12 +6,12 @@ import java.util.List;
 
 public class Constants {
 	
-	public static final String HOMEPAGE_USERNAME="Paul Collings1";
-	public static final String INVALID_CRED_MSG="Invalid credentials1";
+	public static final String HOMEPAGE_USERNAME="Juan zarate";
+	public static final String INVALID_CRED_MSG="Invalid credentials";
 	public static final String RESET_PASSWORD_PAGE_HEADER="Reset Password";
 	
 	
 	public static final List<String> HOMEPAGE_MENU_LINKS = Arrays.asList("Admin","PIM","Leave","Time","Recruitment","My Info","Performance","Dashboard","Directory","Maintenance","Buzz");
 
-	public static final String ADMIN_USERMANAGEMENT_HEADER="Admin\\nUser Management";
+	public static final String ADMIN_USERMANAGEMENT_HEADER="Admin User Management";
 }
